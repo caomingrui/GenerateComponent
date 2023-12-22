06b4dbef4eab3cb72bc4c8cbd18ae72ed022d8b0
mingrui123 <1529539548@qq.com>
2023-12-22 19:31:07 +0800

    feat: 添加CSS变量检索

--- index.js
+++ index.js
@@ -3,22 +3,20 @@ let targetDOM = $0;
 // 打平的选中dom
 let domList = [];
 
-/**
- * 过滤styleSheets依据
- * 根据 属性 | id | class 匹配
- */
-let attributeList = [];
-
 // 命中styleSheets
 let hitStyleSheets = new Map();
 
-let hitStyleSheets2 = new Map();
+// 所有 attrTag( class| id) 字典
 let allDOMAttrMap = new Map();
+
+// attrTag( class| id) 命中rule 字典
+let hitStyleSheets2 = new Map();
+
+// 存储样式 保障样式顺序
+let targetStyle = new Set();
 
 let pendingProcessStyleSheets = document.styleSheets;
 
-let targetStyle = new Set();
-let variableCSS = new Map();
 
 function addDOMAttrMap(attr, dom) {
   let tag = attr.name === 'id'? 'ID': 'CLASS';
@@ -49,6 +47,79 @@ function addTargetDomRecords(dom) {
 }
 
 
+function VariableCSS() {
+  // 存储所需样式变量
+  this.variableCSSMap = new Map();
+  // 暂存匹配变量
+  this.variableList = [];
+  /**
+   * 临时暂存
+   * 针对变量与当前处理StyleSheets同一个文件情况
+   * 变量在处理rule 之前
+   * rule 中变量还未生成
+   */
+  this.variableTemporaryMap = new Map();
+}
+
+VariableCSS.prototype.isVariableCSS = function(rule) {
+  let state = rule.match(/var\(--[\w-]+\)/g);
+  if (!state) return false;
+  this.variableList = state;
+  return true
+}
+
+VariableCSS.prototype.analysisVariableCSS = function(variableList) {
+  if (!variableList || !variableList.length) {
+    variableList = this.variableList;
+  };
+  variableList.forEach((variableStr) => {
+    let variable = variableStr.match(/--[\w-]+/g)[0];
+    if (!this.variableCSSMap.has(variable)) {
+      this.variableCSSMap.set(
+        variable, 
+        this.variableTemporaryMap.get(variable) || null
+      );
+    }
+  });
+  this.variableList = [];
+}
+
+VariableCSS.prototype.matchVariableCSS = function(rule, cb) {
+  if (!rule.styleMap) return rule;
+  if (!rule.cssText.includes('--')) return rule; 
+  // if (!rule.selectorText.includes(':root')) return;
+  let styleMap = [...rule.styleMap];
+  for (let i = 0; i < styleMap.length; i++) {
+    const [key, [val]] = styleMap[i];
+    if (key.slice(0, 2) !== '--') continue;
+    this.variableTemporaryMap.set(key, val[0]);
+    if (this.variableCSSMap.get(key) === null) {
+      cb && cb()
+      this.variableCSSMap.set(key, val[0]);
+    }
+  }
+  return rule;
+}
+
+let variableCSS = new VariableCSS();
+
+function addHitStyleSheets2(attrTag, rule) {
+  if (targetStyle.has(rule)) return;
+
+  // 标记已使用的变量
+  let hasVariableCSSState = variableCSS.isVariableCSS(rule);
+  if (hasVariableCSSState) {
+    variableCSS.analysisVariableCSS();
+  }
+
+  if (hitStyleSheets2.has(attrTag)) {
+    hitStyleSheets2.get(attrTag).push(rule);
+  } else {
+    hitStyleSheets2.set(attrTag, [rule]);
+  }
+  targetStyle.add(rule);
+}
+
 
 function eachTargetDOM(targetDOM) {
   addTargetDomRecords(targetDOM);
@@ -57,6 +128,11 @@ function eachTargetDOM(targetDOM) {
   });
 }
 
+/**
+ * 初次根据（class | id）字符匹配style
+ * 快速检索 styleSheets
+ * @returns 
+ */
 function filterStyleSheets() {
   let styles = document.querySelectorAll("style");
   if (!styles.length) return;
@@ -80,15 +156,11 @@ function filterStyleSheets() {
   }
 }
 
-function addHitStyleSheets2(attrTag, rule) {
-  if (hitStyleSheets2.has(attrTag)) {
-    hitStyleSheets2.get(attrTag).push(rule);
-  } else {
-    hitStyleSheets2.set(attrTag, [rule]);
-  }
-}
 
-
+/**
+ * 处理首次命中的styleSheets
+ * @returns 
+ */
 function matchHitStyleSheets() {
   if (!hitStyleSheets.size) return;
 
@@ -108,6 +180,10 @@ function matchHitStyleSheets() {
 
     for (let j = 0; j < styleRules.length; j ++) {
       const rule = styleRules[j];
+
+      variableCSS.matchVariableCSS(rule, () => {
+        backRuleNumber = defalutBackRuleNumber;
+      });
       /**
        * 命中初始dom属性
        * 以改rule为例 后50个rule没有匹配 其他dom 跳出
@@ -118,24 +194,23 @@ function matchHitStyleSheets() {
       }
       else{
         // 开始 - 目标正常匹配
-        domList.find((dom, ind) => {
+        domList.find(dom => {
           if (dom.matches(rule.selectorText)) {
             addHitStyleSheets2(attrTag, rule.cssText);
-            targetStyle.add(rule.cssText)
             return true
           }
           return false
         })
       }
+
       if (starMatch) {
         backRuleNumber -= 1;
         if (backRuleNumber <= 0) break;
 
-        domList.find((dom, ind) => {
+        domList.find(dom => {
           if (dom.matches(rule.selectorText)) {
             backRuleNumber = defalutBackRuleNumber;
             addHitStyleSheets2(attrTag, rule.cssText);
-            targetStyle.add(rule.cssText)
             return true
           }
           return false;
@@ -146,6 +221,13 @@ function matchHitStyleSheets() {
 }
 
 
+/**
+ * 首次未命中的情况 | 存在未命中dom时
+ * 遍历为匹配过的所有styleSheets
+ * - 外部引用资源
+ * - 动态渲染style
+ * @returns 
+ */
 function checkedDOMList() {
   // 存在未命中的dom 属性
   let unmatchedDOMAttributes = Array.from(allDOMAttrMap, ([attrTag, _]) => ({
@@ -163,7 +245,6 @@ function checkedDOMList() {
   })
   
   for (let i = 0; i < unmatchedStyleSheets.length; i++) {
-    
     const styleSheet = unmatchedStyleSheets[i];
     const rules = styleSheet.cssRules || styleSheet.rules;
     if (!rules) continue;
@@ -178,7 +259,17 @@ function checkedDOMList() {
         if (backRuleNumber <= 0) break;
       }
       
-      unmatchedDOMAttributes.find((records, ind) => {
+      variableCSS.matchVariableCSS(rule, () => {
+        backRuleNumber = defalutBackRuleNumber;
+      });
+
+      /**
+       * 可优化
+       * 举例 unmatchedDOMAttributes -> [a, b, c]
+       * 本次rules a 被命中
+       * 下一轮 a 未命中时 可以考虑将其移至末尾，后续从b 开始, 保障快速匹配
+       */
+      unmatchedDOMAttributes.find(records => {
         const { attrTag, orginDOM } = records;
         const dom = orginDOM;
         if (dom.matches(rule.selectorText)) {
@@ -189,7 +280,6 @@ function checkedDOMList() {
             backRuleNumber = defalutBackRuleNumber;
           }
           addHitStyleSheets2(attrTag, rule.cssText);
-          targetStyle.add(rule.cssText)
           return true
         }
         return false
@@ -199,13 +289,13 @@ function checkedDOMList() {
 }
 
 
-function init() {
+function init(targetDOM) {
   eachTargetDOM(targetDOM);
   filterStyleSheets();
   matchHitStyleSheets();
   checkedDOMList();
-  console.log(allDOMAttrMap);
-  console.log(Array.from(allDOMAttrMap, ([attrTag, _]) => hitStyleSheets2.has(attrTag)).filter(l => l));
+  console.log(targetStyle);
+  console.log(variableCSS.variableCSSMap);
 }
 
-init();
+init(targetDOM);
